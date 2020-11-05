b6df094f2fb12f7ea81b5140c845d670b488c10c
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-11-05 06:18:21 +0000

    comments working

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,12 +1,13 @@
 class PhotosController < ApplicationController
 
   def index
-    @list_of_photos = Photo.all
+    @list_of_photos = Photo.all.order( :created_at => :desc)
     render ({ :template => "photo_templates/index.html.erb"})
   end
 
   def show_photo
-    @list_of_photos = Photo.all
+    photo_url = params.fetch("photo_id")
+    @photo_object = Photo.where({ :id => photo_url}).first()
     render ({ :template => "photo_templates/show_photos.html.erb"})
   end
 
