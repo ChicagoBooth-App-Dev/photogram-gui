04ca0e7aa34e6020f15795943aad662e9f68258d
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-11-05 05:43:16 +0000

    list of photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,12 @@
 class PhotosController < ApplicationController
 
   def index
+    @list_of_photos = Photo.all
     render ({ :template => "photo_templates/index.html.erb"})
   end
 
   def show_photo
+    @list_of_photos = Photo.all
     render ({ :template => "photo_templates/show_photos.html.erb"})
   end
 
